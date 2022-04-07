f4a807e58cfe3523f5426d7cc5c1cc991db49562
minhbac260 <72150717+minhbac260@users.noreply.github.com>
2022-04-08 01:14:53 +0700

    update 8/4 1:11AM

--- LEARNING/broken_keyboard.py
+++ LEARNING/broken_keyboard.py
@@ -1,5 +1,19 @@
-file_inp = "input.INP"
+from numpy import equal
+from pathlib import Path
+
+
+file_inp = str(Path(__file__).parent) + "/input_broken_keyboard.INP"
 
 def process(text_inp):
-    f_pointer = open(file_inp,"r")
-    
+    f_pointer = open(text_inp,"r")
+    char_arr = []
+    key_number = 0
+    text = f_pointer.readline()
+    while text != '0':
+        key_number = int(text)
+        text = f_pointer.readline()
+        print(text)
+        text = f_pointer.readline()
+        
+process(file_inp)
+        
